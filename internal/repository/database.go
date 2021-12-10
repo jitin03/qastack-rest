4b6234c49ae010362186c43417c231573ba4ae94
jitindoriya <jitin.doriya@peak.ai>
2021-12-10 12:21:11 +0530

    update auth host

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -10,17 +10,26 @@ import (
 
 // NewDatabase - returns a pointer to a new database connection
 func NewDatabase() (*gorm.DB, error) {
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbTable := os.Getenv("DB_TABLE")
-	dbPort := os.Getenv("DB_PORT")
+	//dbUsername := os.Getenv("DB_USER")
+	//dbPassword := os.Getenv("DB_PASSWD")
+	//dbHost := os.Getenv("DB_ADDR")
+	//dbPort := os.Getenv("DB_PORT")
+	//
+	//// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + strconv.Itoa(dbPort) + ")/" + dbTable
+	//connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUsername, dbPassword)
+	//fmt.Println(connectionString)
+	dbUser := os.Getenv("DB_USER")
+	dbPasswd := os.Getenv("DB_PASSWD")
+	dbAddr := os.Getenv("DB_ADDR")
+	//dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + strconv.Itoa(dbPort) + ")/" + dbTable
-	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUsername, dbTable, dbPassword)
-	fmt.Println(connectionString)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbAddr, 5432, dbUser, dbPasswd, dbName)
+	fmt.Println(psqlInfo)
 
-	db, con_err := gorm.Open("postgres", "host=localhost port=5433 user=postgres dbname=postgres sslmode=disable password=qastack")
+	db, con_err := gorm.Open("postgres", psqlInfo)
 	if con_err != nil {
 		return db, con_err
 	}
